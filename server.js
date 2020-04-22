33cb9041a046c1fcad20da8ec7a847fb5e2bd716
Glitch (hello-express) <none>
2020-04-22 07:08:02 +0000

    🌻🎷 Checkpoint ./server.js:15107249/2966

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var port = process.env.PORT || 3000,
   gameSocket = null;
 
 var clients = []; // Store Client list
+var playerSpawnPoints = [];
 
 gameSocket = io.on("connection", function(socket) {
   var currentPlayer = {};
@@ -36,6 +37,59 @@ gameSocket = io.on("connection", function(socket) {
       );
     }
   });
+  
+  socket.on('play', function(data) {
+		console.log(currentPlayer.name+' recv: play: '+JSON.stringify(data));
+		// if this is the first person to join the game init the enemies
+    
+    
+		if(clients.length === 0) {
+			playerSpawnPoints = [];
+			data.playerSpawnPoints.forEach(function(_playerSpawnPoint) {
+				var playerSpawnPoint = {
+					position: _playerSpawnPoint.position,
+					rotation: _playerSpawnPoint.rotation
+				};
+				playerSpawnPoints.push(playerSpawnPoint);
+			});
+		}
+
+		var randomSpawnPoint = playerSpawnPoints[Math.floor(Math.random() * playerSpawnPoints.length)];
+		currentPlayer = {
+			name:data.name,
+			position: randomSpawnPoint.position,
+			rotation: randomSpawnPoint.rotation,
+			health: 100
+		};
+		clients.push(currentPlayer);
+		// in your current game, tell you that you have joined
+		console.log(currentPlayer.name+' emit: play: '+JSON.stringify(currentPlayer));
+		socket.emit('play', currentPlayer);
+		// in your current game, we need to tell the other players about you.
+		socket.broadcast.emit('other player connected', currentPlayer);
+	});
+  
+  socket.on('player move', function(data) {
+		console.log('recv: move: '+JSON.stringify(data));
+		currentPlayer.position = data.position;
+		socket.broadcast.emit('player move', currentPlayer);
+	});
+
+	socket.on('player turn', function(data) {
+		console.log('recv: turn: '+JSON.stringify(data));
+		currentPlayer.rotation = data.rotation;
+		socket.broadcast.emit('player turn', currentPlayer);
+	});
+  
+  socket.on('player shoot', function() {
+		console.log(currentPlayer.name+' recv: shoot');
+		var data = {
+			name: currentPlayer.name
+		};
+		console.log(currentPlayer.name+' bcst: shoot: '+JSON.stringify(data));
+		socket.emit('player shoot', data);
+		socket.broadcast.emit('player shoot', data);
+	});
  
   socket.on('disconnect', function() {
 		console.log(currentPlayer.name+' recv: disconnect '+currentPlayer.name);
