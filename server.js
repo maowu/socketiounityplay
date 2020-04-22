3d802f738b81c90bfaae4fd963fcaf10fb26ac2a
Glitch (hello-express) <none>
2020-04-22 07:54:02 +0000

    🏎🐟 Checkpoint ./server.js:15107249/18

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ gameSocket = io.on("connection", function(socket) {
   });
   
   socket.on('play', function(data) {
-		console.log(currentPlayer.name+' recv: play: '+JSON.stringify(data));
+		console.log(currentPlayer.name + ' recv: play: ' + JSON.stringify(data));
 		// if this is the first person to join the game init the enemies
     
     
@@ -63,38 +63,38 @@ gameSocket = io.on("connection", function(socket) {
 		};
 		clients.push(currentPlayer);
 		// in your current game, tell you that you have joined
-		console.log(currentPlayer.name+' emit: play: '+JSON.stringify(currentPlayer));
+		console.log(currentPlayer.name + ' emit: play: ' + JSON.stringify(currentPlayer));
 		socket.emit('play', currentPlayer);
 		// in your current game, we need to tell the other players about you.
 		socket.broadcast.emit('other player connected', currentPlayer);
 	});
   
   socket.on('player move', function(data) {
-		console.log('recv: move: '+JSON.stringify(data));
+		console.log('recv: move: '+ JSON.stringify(data));
 		currentPlayer.position = data.position;
 		socket.broadcast.emit('player move', currentPlayer);
 	});
 
 	socket.on('player turn', function(data) {
-		console.log('recv: turn: '+JSON.stringify(data));
+		console.log('recv: turn: '+ JSON.stringify(data));
 		currentPlayer.rotation = data.rotation;
 		socket.broadcast.emit('player turn', currentPlayer);
 	});
   
   socket.on('player shoot', function() {
-		console.log(currentPlayer.name+' recv: shoot');
+		console.log(currentPlayer.name +' recv: shoot');
 		var data = {
 			name: currentPlayer.name
 		};
-		console.log(currentPlayer.name+' bcst: shoot: '+JSON.stringify(data));
+		console.log(currentPlayer.name +' bcst: shoot: '+JSON.stringify(data));
 		socket.emit('player shoot', data);
 		socket.broadcast.emit('player shoot', data);
 	});
  
   socket.on('disconnect', function() {
-		console.log(currentPlayer.name+' recv: disconnect '+currentPlayer.name);
+		console.log(currentPlayer.name +' recv: disconnect '+ currentPlayer.name);
 		socket.broadcast.emit('other player disconnected', currentPlayer);
-		console.log(currentPlayer.name+' bcst: other player disconnected '+JSON.stringify(currentPlayer));
+		console.log(currentPlayer.name +' bcst: other player disconnected '+ JSON.stringify(currentPlayer));
 		for(var i=0; i<clients.length; i++) {
 			if(clients[i].name === currentPlayer.name) {
 				clients.splice(i,1);
